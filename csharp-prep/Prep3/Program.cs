328d6742d8c8aeb37e0d548904a79456ac595f6a
marvinagc <marvingaliciaxd40@gmail.com>
2023-06-09 13:31:59 -0700

    Program has in it implemented the stretch challenges.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,79 @@
 using System;
 
+public static class Game
+{
+    public static void playGame()
+    {
+                // Create a guess my number game.
+        // User will provide the number to be guessed.
+
+
+        // im adding other functionality, thusthe following is commented out as demo that core requirements are met.
+            // Console.Write("What is the magic number? ");
+            // string magicNumber = Console.ReadLine();
+            //int answer = int.Parse(magicNumber);
+        
+
+
+        Random random = new Random();
+        int randomNumber = random.Next(0, 100);
+        //Console.WriteLine(randomNumber);
+
+        // If the number guessed is less than the magic number, print "Higher."
+        // If the number guessed is greater than the magic number, print "Lower."
+
+        bool gotIt = false;    
+        int guessCount = 0;
+        
+
+
+        while (!gotIt)
+        {
+            // User wil type the number guessed.
+            Console.Write("What is your guess? ");
+            string numberGuessed = Console.ReadLine();
+            int guess = int.Parse(numberGuessed);
+        
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Congrats.");
+                gotIt = true;
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("Higher.");
+            }
+            guessCount++;
+        }
+        Console.WriteLine($"You guessed {guessCount} times.");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = false;
+        Console.WriteLine("Welcome to the Guess My Number Game.");
+
+        while (!playAgain)
+        {
+            Game.playGame();
+            Console.Write("Would you like to play again? (y/n) ");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                playAgain = false;
+            }
+            else
+            {
+                playAgain = true;
+            }
+        }
+        Console.WriteLine("Thanks for playing.");
     }
 }
