9534f03adc662aa577b340e99677ed251d7a2560
marvinagc <marvingaliciaxd40@gmail.com>
2023-06-10 00:41:01 -0700

    most of the program completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
+    // Function will displau a welcome message.
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function will ask user for their name.
+    static string AskForName()
+    {
+        Console.WriteLine("Please enter your name?: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // User will be asked for their age.
+    static int AskForAge()
+    {
+        Console.WriteLine("What is your age?: ");
+        string age = Console.ReadLine();
+        int ageNumber = int.Parse(age);
+        return ageNumber;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = AskForName();
+        double squaredNumber = Math.Pow(AskForAge(), 2);
+        Console.WriteLine($"{name}, the square of your age is {squaredNumber}.");
     }
 }
